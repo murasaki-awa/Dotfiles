bd1ac02d837607e437f6c6965f6f69f8ff0e297c
murasaki-awa <murasaki.awa@proton.me>
2026-03-10 21:20:31 +0545

    feat: neo-tree now auto-closes on file selection

--- nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -25,5 +25,16 @@ return {
         },
       },
     },
+    event_handlers = {
+      {
+        event = 'file_open_requested',
+        handler = function()
+          -- auto close
+          -- vim.cmd("Neotree close")
+          -- OR
+          require('neo-tree.command').execute { action = 'close' }
+        end,
+      },
+    },
   },
 }
