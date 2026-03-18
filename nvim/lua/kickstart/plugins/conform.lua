48ce7f7d730d404f145dfc07e7bedb80dc815932
murasaki-awa <murasaki.awa@proton.me>
2026-03-18 19:26:19 +0545

    feat: added formatter for various files

--- nvim/lua/kickstart/plugins/conform.lua
+++ nvim/lua/kickstart/plugins/conform.lua
@@ -17,7 +17,7 @@ return {
   ---@module 'conform'
   ---@type conform.setupOpts
   opts = {
-    notify_on_error = false,
+    notify_on_error = true,
     format_on_save = function(bufnr)
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
@@ -34,9 +34,23 @@ return {
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
+
+      terraform = { 'terraform_fmt' },
+      ['terrfaform-vars'] = { 'terraform_fmt' },
+
+      angular = { 'prettier' },
+      css = { 'prettier' },
+      html = { 'prettier' },
+      json = { 'prettier' },
+      less = { 'prettier' },
+      markdown = { 'prettier' },
+      scss = { 'prettier' },
+      typescript = { 'prettier' },
+      yaml = { 'prettier' },
+
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
-      --
+
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
     },
