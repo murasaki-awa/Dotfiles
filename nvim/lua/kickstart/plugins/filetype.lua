374757a331c12a341480cd53156db97283be86a3
murasaki-awa <murasaki.awa@proton.me>
2026-03-18 19:27:42 +0545

    feat: file pattern matching for ansible

--- nvim/lua/kickstart/plugins/filetype.lua
+++ nvim/lua/kickstart/plugins/filetype.lua
@@ -2,6 +2,33 @@
 ---@type LazySpec
 return {
   vim.filetype.add {
+    pattern = {
+      -- ---------------------------
+      -- filetype: yaml.ansible
+      -- ---------------------------
+
+      ['.*/ansible/.*%yml'] = 'yaml.ansible',
+      ['.*/ansible/.*%yaml'] = 'yaml.ansible',
+
+      ['.*/playbooks/.*%yml'] = 'yaml.ansible',
+      ['.*/playbooks/.*%yaml'] = 'yaml.ansible',
+
+      ['.*/roles/.*/tasks/.*%yml'] = 'yaml.ansible',
+      ['.*/roles/.*/tasks/.*%yaml'] = 'yaml.ansible',
+
+      ['.*/roles/.*/handlers/.*%yml'] = 'yaml.ansible',
+      ['.*/roles/.*/defaults/.*%yml'] = 'yaml.ansible',
+      ['.*/roles/.*/vars/.*%yml'] = 'yaml.ansible',
+      ['.*/roles/.*/handlers/.*%yaml'] = 'yaml.ansible',
+      ['.*/roles/.*/defaults/.*%yaml'] = 'yaml.ansible',
+      ['.*/roles/.*/vars/.*%yaml'] = 'yaml.ansible',
+
+      -- ---------------------------
+      -- filetype: terraform
+      -- ---------------------------
+
+      -- ['.*/terraform/*.tf'] = 'terraform',
+    },
     extension = {
       tf = 'terraform',
     },
