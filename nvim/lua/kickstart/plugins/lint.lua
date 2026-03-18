8dcda05dfe07645ced65bd8ea8358130e95357a9
murasaki-awa <murasaki.awa@proton.me>
2026-03-18 19:29:26 +0545

    feat: added lint for files

--- nvim/lua/kickstart/plugins/lint.lua
+++ nvim/lua/kickstart/plugins/lint.lua
@@ -6,8 +6,9 @@ return {
   config = function()
     local lint = require 'lint'
     lint.linters_by_ft = {
-      markdown = { 'markdownlint' },
+      markdown = { 'vale' },
       terraform = { 'tflint' },
+      ['yaml.ansible'] = { 'ansible_lint' },
     }
 
     -- To allow other plugins to add linters to require('lint').linters_by_ft,
@@ -31,14 +32,9 @@ return {
     -- }
     --
     -- You can disable the default linters by setting their filetypes to nil:
-    -- lint.linters_by_ft['clojure'] = nil
     -- lint.linters_by_ft['dockerfile'] = nil
-    -- lint.linters_by_ft['inko'] = nil
-    -- lint.linters_by_ft['janet'] = nil
     -- lint.linters_by_ft['json'] = nil
     -- lint.linters_by_ft['markdown'] = nil
-    -- lint.linters_by_ft['rst'] = nil
-    -- lint.linters_by_ft['ruby'] = nil
     -- lint.linters_by_ft['terraform'] = nil
     -- lint.linters_by_ft['text'] = nil
 
