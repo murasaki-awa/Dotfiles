2651207aa59c1fa134f87b1da8e74e358023d724
murasaki-awa <murasaki.awa@proton.me>
2026-08-20 19:58:24 +0545

    chore: commented out vale

--- nvim/lua/kickstart/plugins/lint.lua
+++ nvim/lua/kickstart/plugins/lint.lua
@@ -7,7 +7,7 @@ return {
     local lint = require 'lint'
     lint.linters_by_ft = {
       dockerfile = { 'hadolint' },
-      markdown = { 'vale' },
+      -- markdown = { 'vale' },
       terraform = { 'tflint' },
       ['yaml.ansible'] = { 'ansible_lint' },
     }
