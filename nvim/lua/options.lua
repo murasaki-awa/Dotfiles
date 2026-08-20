0132e6b1c4780db1397c793d4818463a7f3e3471
murasaki-awa <murasaki.awa@proton.me>
2026-08-20 19:59:06 +0545

    feat: added cursor blink to nvim

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -67,4 +67,13 @@ vim.o.scrolloff = 10
 -- instead raise a dialog asking if you wish to save the current file(s)
 vim.o.confirm = true
 
+vim.opt.guicursor = {
+  'n-v-c:block',
+  'i-ci-ve:ver25',
+  'r-cr:hor20',
+  'o:hor50',
+  'a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor',
+  'sm:block-blinkwait175-blinkoff150-blinkon175',
+}
+
 -- ------------------------------------------------------------------------
